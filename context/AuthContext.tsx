7958671a96d634ecb4892c120d6b22e6d81d420a
tomasbagu <tomasbagu@unisabana.edu.co>
2025-03-24 12:08:42 -0500

    POSappInitial

---
+++ context/AuthContext.tsx
@@ -0,0 +1,35 @@
+import { User } from "@/interfaces/common";
+import { createContext } from "react";
+
+export const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }: any) => {
+
+    const login = (email: string, password: string) => {
+
+
+    }
+
+    const logout = () => {
+
+    }
+
+    const register = (user: User) => {
+
+    }
+
+    const updateUser = (user: User) => {
+
+    }
+
+    const updateRole = (role:  "client" | "chef" | "cashier") => {
+        
+    }
+
+    return (
+        <AuthContext.Provider value={{login, logout, register, updateUser, updateRole}}>
+        {children}
+        </AuthContext.Provider>
+    );
+}
+
