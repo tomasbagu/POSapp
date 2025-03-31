fde651fba47764fc0e3c2b03e86c82f8191532ba
tomasbagu <tomasbagu@unisabana.edu.co>
2025-03-31 14:52:58 -0500

    Refactor layout and index components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,11 @@
 import { AuthProvider } from '@/context/AuthContext';
 import { Stack } from 'expo-router';
-import { ActivityIndicator, View } from 'react-native';
 import 'react-native-reanimated';
-import { useFonts } from "expo-font";
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
-    "Inter-Regular": require("@/assets/fonts/Inter.ttf"),
-    "Inter-Italic": require("@/assets/fonts/Inter-Italic.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  }
   return (
     <AuthProvider>
       <Stack>
-        <Stack.Screen name="auth" />
-        <Stack.Screen name="(app)" />
       </Stack>
     </AuthProvider>);
-} 
-
-    
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,53 +1,16 @@
-import { Redirect, Slot } from "expo-router";
-    import { useContext, useEffect, useState } from "react";
-    import { AuthContext } from "@/context/AuthContext";
-    import { doc, getDoc } from "firebase/firestore";
-    import { db } from "@/utils/Firebase";
-    import { View, ActivityIndicator } from "react-native";
-    
-    export default function RootLayout() {
-      const { currentUser } = useContext(AuthContext);
-      const [role, setRole] = useState<string | null>(null);
-      const [loading, setLoading] = useState(true);
-    
-      useEffect(() => {
-        const fetchRole = async () => {
-          if (!currentUser) {
-            setLoading(false);
-            return;
-          }
-    
-          try {
-            const snap = await getDoc(doc(db, "users", currentUser.uid));
-            if (snap.exists()) {
-              setRole(snap.data()?.role);
-            }
-          } catch (err) {
-            console.error("Error obteniendo el rol:", err);
-          } finally {
-            setLoading(false);
-          }
-        };
-    
-        fetchRole();
-      }, [currentUser]);
-    
-      if (loading) {
-        return (
-          <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <ActivityIndicator size="large" />
-          </View>
-        );
-      }
-    
-    
-      if (!currentUser) return <Redirect href="/auth/login" />;
-    
-      if (role === "client") return <Redirect href="/(app)/Client/homeClient" />;
-      if (role === "chef") return <Redirect href="/(app)/Chef/homeChef" />;
-      if (role === "cashier") return <Redirect href="/(app)/Cashier/homeCash" />;
-    
-      
-      return <Slot />;
-    }
-    
+import { useEffect } from "react";
+import { useRouter } from "expo-router";
+
+export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/auth/login"); // Redirige a login directamente
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return null; // No renderiza nada mientras espera
+}
